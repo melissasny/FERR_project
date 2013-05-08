c193735e5d74fe75a4defc60add55f00d4f4440c
Melissa Shields <mshieldsny@gmail.com>
2013-05-08 15:15:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FERRProject::Application.config.secret_token = 'aa6c1683be4195133728f50754913a0803fd2fa2512c8ea1d643168a877eecacc0b88aac974c44dddc0d300948f0941f8e0f7dbdc78631d99e68b06e7c66195f'
