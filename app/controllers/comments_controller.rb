ef3caa862cc3995b791cee5622b8736d00e49655
Melissa Shields <mshieldsny@gmail.com>
2013-05-11 11:59:23 -0400

    Update new comment creation process

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,13 @@
 class CommentsController < ApplicationController
+  before_filter :ensure_user_is_logged_in, :only => [:new, :create]
+
   def new
   	@comment = Comment.new
   end
 
-def create
+  def create
   	@comment = Comment.new(params[:comment])
+    @comment.user_id = current_user.id
   	if @comment.save 
   		redirect_to log_in_path
   	else
@@ -13,5 +16,12 @@ def create
   end
 
   def index
+  end
+
+  private
+  def ensure_user_is_logged_in
+    unless current_user
+      redirect_to root_url, alert: "Please log-in to comment. First-time visitors please sign-up."
+    end  
   end
 end
