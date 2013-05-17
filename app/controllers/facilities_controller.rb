e04910fbd4c8296cc194b14961834af141cad4f5
Melissa Shields <mshieldsny@gmail.com>
2013-05-17 19:03:02 -0400

    Change number of comments per page in facility show view

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -11,8 +11,6 @@ class FacilitiesController < ApplicationController
 	def show
 		@facility = Facility.find(params[:id])
 		require 'will_paginate/array'
-#Sort comments for the selected facility in reverse chronological order based on date and time created
-		@facility_comments = @facility.comments.paginate(:page => params[:page], :per_page => 5)
-		@sorted_facility_comments = @facility_comments.sort_by {|created_at, datetime| datetime}.reverse
+		@facility_comments = @facility.comments.paginate(:page => params[:page], :per_page => 3)
 	end
 end
