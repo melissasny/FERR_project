bb8e134bb306e0548e64555f9cd6c76701a980a4
Melissa Shields <mshieldsny@gmail.com>
2013-06-10 17:19:43 -0400

    Ensure user email uniqueness by creating a database index on the email field and requiring that field to be unique.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130509170529) do
+ActiveRecord::Schema.define(:version => 20130610211234) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -73,5 +73,7 @@ ActiveRecord::Schema.define(:version => 20130509170529) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
