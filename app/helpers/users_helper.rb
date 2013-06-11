3fc06e4e6a2e8abcffbe5d4388bc9281aaccfe39
Melissa Shields <mshieldsny@gmail.com>
2013-06-11 16:32:10 -0400

    Add user gravatar to facilities show view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 
 	def gravatar_for(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}?d=identicon"
+		gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}?d=wavatar"
 		image_tag(gravatar_url, alt: user.user_name)
 	end
 
