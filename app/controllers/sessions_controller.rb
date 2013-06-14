18fe873ed54a340dbbd6aaaaf1deeb00af4e38d3
Melissa Shields <mshieldsny@gmail.com>
2013-06-14 15:09:10 -0400

    Add auth_token to app controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.authenticate(params[:user_name].downcase, params[:password])
   	if user
-      cookies.permanent[:auth_token] = user.auth_token
-#  		session[:user_id] = user.id 
+      if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
   		redirect_to root_url
   	else
   		flash.now.alert = "Oops! Invalid user name or password"
@@ -16,7 +19,6 @@ class SessionsController < ApplicationController
 
   def destroy
     cookies.delete(:auth_token)
-#   session[:user_id] = nil
     redirect_to root_url
   end
 
