e5914d6c9a198ae586a469f560f6c8d3a2d60823
Melissa Shields <mshieldsny@gmail.com>
2013-06-19 16:54:44 -0400

    Correct action mailer default url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ FERRProject::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => "http://www.pacific-springs-5481.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "www.pacific-springs-5481.herokuapp.com" }
 
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
