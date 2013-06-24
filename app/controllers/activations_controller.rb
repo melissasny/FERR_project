e48b25618526396b8d6c8f5dfaf482d5bdb9f405
Melissa Shields <mshieldsny@gmail.com>
2013-06-24 17:41:50 -0400

    Create activations controller and update method

---
+++ app/controllers/activations_controller.rb
@@ -0,0 +1,14 @@
+class ActivationsController < ApplicationController
+
+	def update
+   	 @user = User.find_by_activation_token!(params[:id])
+    	if @user.activation_sent_at < 2.hours.ago
+      		@user.destroy
+      		redirect_to sign_up_path, :alert => "Your sign-up activation authorization has expired. Please sign-up again."
+    	elsif @user.update_attribute(:active, true)
+      		redirect_to log_in_path, :notice => "Your sign-up has been successfully activated!"
+    	else
+      		redirect to sign_up_path
+    	end
+	end
+end
