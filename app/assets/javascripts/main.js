db1c840f96df697b6da2a8157e275e6cf5446222
Melissa Shields <mshieldsny@gmail.com>
2013-07-11 19:27:15 -0400

    Add map code for the Dance button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 	var grp1 = {title:'Make A Splash', code:'<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=207352579320501097310.0004d6e3b22526a73bd44&amp;ie=UTF8&amp;t=m&amp;ll=40.778722,-73.969574&amp;spn=0.129988,0.145912&amp;z=12&amp;output=embed"></iframe>'};
 	var grp2 = {title: 'Pedal to the Metal', code:'<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=207352579320501097310.0004d737dd42c1c37f807&amp;ie=UTF8&amp;t=m&amp;ll=40.749598,-73.982964&amp;spn=0.130045,0.145912&amp;z=12&amp;output=embed"></iframe>'};
 	var grp5 = {title: 'Find Your Bliss', code: '<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=207352579320501097310.0004dceba5fbc153366aa&amp;ie=UTF8&amp;t=m&amp;ll=40.7587,-73.983307&amp;spn=0.130028,0.145912&amp;z=12&amp;output=embed"></iframe>'};
+	var grp6 = {title: 'Dance Your \#$% Off!', code: '<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=207352579320501097310.0004e142e0d56128a647f&amp;ie=UTF8&amp;t=m&amp;ll=40.763381,-73.977814&amp;spn=0.130018,0.145912&amp;z=12&amp;output=embed"></iframe>'};
 	var currAct;
 	var temp;
 	
@@ -20,6 +21,8 @@ $(document).ready(function(){
 			case "grp2": currAct = grp2;
 				break;
 			case "grp5": currAct = grp5;
+				break;
+			case "grp6": currAct = grp6;
 		}
 		$('#flexible-container').html(currAct.code);
 		$('#map h2').text(currAct.title);
